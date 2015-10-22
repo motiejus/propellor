9c308fd371799f58c1217648cd1d68b6b27d64cc
Joey Hess <joeyh@joeyh.name>
2015-10-22 18:59:16 -0400

    belt-and-suspenders check of kpartx output

--- src/Propellor/Property/Partition.hs
+++ src/Propellor/Property/Partition.hs
@@ -9,7 +9,6 @@ import Utility.Applicative
 
 import System.Posix.Files
 import Data.List
-import Data.Char
 
 -- | Filesystems etc that can be used for a partition.
 data Fs = EXT2 | EXT3 | EXT4 | BTRFS | REISERFS | XFS | FAT | VFAT | NTFS | LinuxSwap
@@ -53,6 +52,15 @@ data LoopDev = LoopDev
 	, wholeDiskLoopDev :: FilePath -- ^ corresponding device for the whole loop disk
 	} deriving (Show)
 
+isLoopDev :: LoopDev -> IO Bool
+isLoopDev l = isLoopDev' (partitionLoopDev l) <&&> isLoopDev' (wholeDiskLoopDev l)
+
+isLoopDev' :: FilePath -> IO Bool
+isLoopDev' f
+	| "loop" `isInfixOf` f = catchBoolIO $
+		isBlockDevice <$> getFileStatus f
+	| otherwise = return False
+
 -- | Uses the kpartx utility to create device maps for partitions contained
 -- within a disk image file. The resulting loop devices are passed to the
 -- property, which can operate on them. Always cleans up after itself,
@@ -63,8 +71,12 @@ kpartx diskimage mkprop = go `requires` Apt.installed ["kpartx"]
 	go :: Property DebianLike
 	go = property' (getDesc (mkprop [])) $ \w -> do
 		cleanup -- idempotency
-		s <- liftIO $ readProcess "kpartx" ["-avs", diskimage]
-		r <- ensureProperty (mkprop (kpartxParse s))
+		loopdevs <- liftIO $ kpartxParse
+			<$> readProcess "kpartx" ["-avs", diskimage]
+		bad <- liftIO $ filterM (not <$$> isLoopDev) loopdevs
+		unless (null bad) $
+			error $ "kpartx output seems to include non-loop-devices (possible parse failure): " ++ show bad
+		r <- ensureProperty (mkprop loopdevs)
 		cleanup
 		return r
 	cleanup = void $ liftIO $ boolSystem "kpartx" [Param "-d", File diskimage]
