88dc827519e3b686c151677ccad256cb5c6143a5
Joey Hess <joeyh@joeyh.name>
2015-10-23 13:06:38 -0400

    propellor spin

--- src/Propellor/Property/DiskImage.hs
+++ src/Propellor/Property/DiskImage.hs
@@ -175,7 +175,7 @@ partitionsPopulated chrootdir mnts devs = property desc $ mconcat $ zipWith go m
 			[ Include (Pattern m)
 			, Exclude (filesUnder m)
 			-- Preserve any lost+found directory that mkfs made
-			, Exclude (Pattern "lost+found")
+			, Protect (Pattern "lost+found")
 			]) childmnts
 
 -- The constructor for each Partition is passed the size of the files
