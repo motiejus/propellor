35a314d72178a00e7487553bf60173439aa00b81
Jelmer Vernooij <jelmer@jelmer.uk>
2016-01-02 16:44:15 +0000

    Add Munin module.
    Signed-off-by: Jelmer Vernooij <jelmer@jelmer.uk>

--- src/Propellor/Property/Munin.hs
+++ src/Propellor/Property/Munin.hs
@@ -19,19 +19,19 @@ import qualified Propellor.Property.Service as Service
 nodePort :: Integer
 nodePort = 4949
 
-nodeInstalled :: Property DebianLike
+nodeInstalled :: Property NoInfo
 nodeInstalled = Apt.serviceInstalledRunning "munin-node"
 
-nodeRestarted :: Property DebianLike
+nodeRestarted :: Property NoInfo
 nodeRestarted = Service.restarted "munin-node"
 
 nodeConfPath :: FilePath
 nodeConfPath = "/etc/munin/munin-node.conf"
 
-masterInstalled :: Property DebianLike
+masterInstalled :: Property NoInfo
 masterInstalled = Apt.serviceInstalledRunning "munin"
 
-masterRestarted :: Property DebianLike
+masterRestarted :: Property NoInfo
 masterRestarted = Service.restarted "munin"
 
 masterConfPath :: FilePath
@@ -46,10 +46,11 @@ hostListFragment' hs os = concatMap muninHost hs
   where
 	muninHost :: Host -> [String]
 	muninHost h = [ "[" ++ (hostName h) ++ "]"
-		      , "  address " ++ maybe (hostName h) (val . fst) (hOverride h)
-		      ] ++ (maybe [] (\x -> ["  port " ++ (val $ snd x)]) (hOverride h)) ++ [""]
+		      , "  address " ++ maybe (hostName h) (fromIPAddr . fst) (hOverride h)
+		      ] ++ (maybe [] (\x -> ["  port " ++ (show $ fromPort $ snd x)]) (hOverride h)) ++ [""]
 	hOverride :: Host -> Maybe (IPAddr, Port)
 	hOverride h = lookup (hostName h) os
+	fromPort (Port p) = p
 
 -- | Create the host list fragment for master config.
 hostListFragment :: [Host] -> [String]
