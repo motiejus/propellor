26d7789b0616af30d45defcf299c251c786f7572
Joey Hess <joeyh@joeyh.name>
2016-02-08 11:18:17 -0400

    Allow using combineProperties and propertyList with lists of RevertableProperty.

--- src/Propellor/Property/List.hs
+++ src/Propellor/Property/List.hs
@@ -56,21 +56,40 @@ class PropertyList l where
 	-- use `props`
 	propertyList :: Desc -> l -> Property (PropertyListType l)
 
--- | Combines a list of properties, resulting in one property that
--- ensures each in turn. Stops if a property fails.
---
--- > combineProperties "foo" $ props
--- > 	& bar
--- > 	& baz
---
--- This is similar to using `mconcat` with a list of properties,
--- except it can combine together different types of properties.
-combineProperties :: SingI metatypes => Desc -> Props (MetaTypes metatypes) -> Property (MetaTypes metatypes)
-combineProperties desc (Props ps) = 
-	property desc (combineSatisfy cs NoChange)
-		`addChildren` cs
-  where
-	cs = map toChildProperty ps
+	-- | Combines a list of properties, resulting in one property that
+	-- ensures each in turn. Stops if a property fails.
+	combineProperties :: Desc -> l -> Property (PropertyListType l)
+
+-- | Type level function to calculate whether a PropertyList has Info.
+type family PropertyListType t
+type instance PropertyListType [Property HasInfo] = HasInfo
+type instance PropertyListType [Property NoInfo] = NoInfo
+type instance PropertyListType [RevertableProperty NoInfo] = NoInfo
+type instance PropertyListType [RevertableProperty HasInfo] = HasInfo
+type instance PropertyListType PropList = HasInfo
+
+instance PropertyList [Property NoInfo] where
+	propertyList desc ps = simpleProperty desc (ensureProperties ps) ps
+	combineProperties desc ps = simpleProperty desc (combineSatisfy ps NoChange) ps
+
+instance PropertyList [Property HasInfo] where
+	-- It's ok to use ignoreInfo here, because the ps are made the
+	-- child properties of the property, and so their info is visible
+	-- that way.
+	propertyList desc ps = infoProperty desc (ensureProperties $ map ignoreInfo ps) mempty ps
+	combineProperties desc ps = infoProperty desc (combineSatisfy ps NoChange) mempty ps
+
+instance PropertyList [RevertableProperty HasInfo] where
+	propertyList desc ps = propertyList desc (map setupRevertableProperty ps)
+	combineProperties desc ps = combineProperties desc (map setupRevertableProperty ps)
+
+instance PropertyList [RevertableProperty NoInfo] where
+	propertyList desc ps = propertyList desc (map setupRevertableProperty ps)
+	combineProperties desc ps = combineProperties desc (map setupRevertableProperty ps)
+
+instance PropertyList PropList where
+	propertyList desc = propertyList desc . getProperties
+	combineProperties desc = combineProperties desc . getProperties
 
 combineSatisfy :: [ChildProperty] -> Result -> Propellor Result
 combineSatisfy [] rs = return rs
