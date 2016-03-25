df6e4423d5a7cdbe535865fd865b497f1da48674
Joey Hess <joeyh@joeyh.name>
2016-03-25 17:24:54 -0400

    Avoid generating excessively long paths to the unix socket file used for ssh connection caching.
    Mostly. Can still generate a too long one if $HOME is longer than 60 bytes.

--- src/Propellor/Ssh.hs
+++ src/Propellor/Ssh.hs
@@ -66,11 +66,11 @@ socketFile home hn = selectSocketFile
 	md5 = take 9 $ MD5.md5s $ MD5.Str hn
 
 selectSocketFile :: [FilePath] -> FilePath -> FilePath
-selectSocketFile [] d = d
+selectSocketFile [] fallback = fallback
 selectSocketFile [f] _ = f
-selectSocketFile (f:fs) d
+selectSocketFile (f:fs) fallback
 	| valid_unix_socket_path f = f
-	| otherwise = selectSocketFile fs d
+	| otherwise = selectSocketFile fs fallback
 
 valid_unix_socket_path :: FilePath -> Bool
 valid_unix_socket_path f = length (decodeW8 f) < 100 - reservedbyssh
