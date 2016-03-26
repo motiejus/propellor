843151422e1c689bdcaeb3e0d743c60c40fecb5a
Joey Hess <joeyh@joeyh.name>
2016-03-26 15:35:55 -0400

    remove `os` property
    The new properties let the type checker know what the target OS is.

--- src/Propellor/Info.hs
+++ src/Propellor/Info.hs
@@ -1,12 +1,9 @@
-{-# LANGUAGE PackageImports, TypeFamilies, DataKinds, PolyKinds #-}
+{-# LANGUAGE PackageImports #-}
 
 module Propellor.Info (
 	osDebian,
 	osBuntish,
-	osArchLinux,
 	osFreeBSD,
-	setInfoProperty,
-	addInfoProperty,
 	pureInfoProperty,
 	pureInfoProperty',
 	askInfo,
@@ -67,11 +64,32 @@ osDebian = osDebian' Linux
 osDebian' :: DebianKernel -> DebianSuite -> Architecture -> Property (HasInfo + Debian)
 osDebian' kernel suite arch = tightenTargets $ os (System (Debian kernel suite) arch)
 
+-- | Specifies that a host's operating system is Debian,
+-- and further indicates the suite and architecture.
+-- 
+-- This provides info for other Properties, so they can act
+-- conditionally on the details of the OS.
+--
+-- It also lets the type checker know that all the properties of the
+-- host must support Debian.
+--
+-- > & osDebian (Stable "jessie") "amd64"
+osDebian :: DebianSuite -> Architecture -> Property (HasInfo + Debian)
+osDebian suite arch = tightenTargets $ os (System (Debian suite) arch)
+
 -- | Specifies that a host's operating system is a well-known Debian
 -- derivative founded by a space tourist.
 --
--- This only provides info for other Properties, so they can act
--- conditionally on the os.
+-- (The actual name of this distribution is not used in Propellor per
+-- <http://joeyh.name/blog/entry/trademark_nonsense/>)
+osBuntish :: Release -> Architecture -> Property (HasInfo + Buntish)
+osBuntish release arch = tightenTargets $ os (System (Buntish release) arch)
+
+-- | Specifies that a host's operating system is FreeBSD
+-- and further indicates the release and architecture.
+osFreeBSD :: FreeBSDRelease -> Architecture -> Property (HasInfo + FreeBSD)
+osFreeBSD release arch = tightenTargets $ os (System (FreeBSD release) arch)
+
 os :: System -> Property (HasInfo + UnixLike)
 os system = pureInfoProperty ("Operating " ++ show system) (InfoVal system)
 
@@ -147,6 +165,3 @@ getAddresses = mapMaybe getIPAddr . S.toList . fromDnsInfo . fromInfo
 
 hostAddresses :: HostName -> [Host] -> [IPAddr]
 hostAddresses hn hosts = maybe [] (getAddresses . hostInfo) (findHost hosts hn)
-
-addHostInfo ::IsInfo v => Host -> v -> Host
-addHostInfo h v = h { hostInfo = addInfo (hostInfo h) v }
