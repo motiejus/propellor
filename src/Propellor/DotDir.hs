d88bec6c6bf496f5d43e3d2110e40c328f8411ff
Joey Hess <joeyh@joeyh.name>
2017-03-01 16:50:40 -0400

    Fixed https url to propellor git repository.

--- src/Propellor/DotDir.hs
+++ src/Propellor/DotDir.hs
@@ -50,7 +50,7 @@ upstreambranch = "upstream/master"
 -- Using the github mirror of the main propellor repo because
 -- it is accessible over https for better security.
 netrepo :: String
-netrepo = "https://github.com/joeyh/propellor.git"
+netrepo = "https://git.joeyh.name/git/propellor.git"
 
 dotPropellor :: IO FilePath
 dotPropellor = do
